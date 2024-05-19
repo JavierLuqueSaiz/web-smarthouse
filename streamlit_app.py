b0d99e89b1ba7030fc37a50d28e692092e6ca28e
JavierLuqueSaiz <167310505+JavierLuqueSaiz@users.noreply.github.com>
2024-05-19 18:29:03 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -232,103 +232,40 @@ if button_pressed:
     c_ascendente = costes.cumsum()
     c_total = costes.sum()
 
-    fig, ax = plt.subplots()
+    fig, (ax1,ax2) = plt.subplots(1,2)
 
-    ax.plot(dates_test, costes, label='Cost')
-    ax.plot(dates_test, c_ascendente, label='Cumulative')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('€', rotation=0)
-    ax.set_title('Cost Predicted')
-    ax.yaxis.set_label_coords(-0.1,0.5)
-    ax.legend()
-    ax.tick_params(axis="x", labelrotation=45)
+    if len(dates_test) > 60:
+
+        df_r = pd.DataFrame({'fecha': dates_test, 'valor': costes})
+        df_rr = pd.DataFrame(columns=["fecha","valor","suma_acumulada"])
+        s = 0
+        size = len(df_r)
+        n = size // 30
+        while s+n < size:
+            portion = df_r.iloc[s:s+n]
+            s += n
+            portion['suma_acumulada'] = portion['valor'].cumsum()
+            df_rr = pd.concat([df_rr, portion.iloc[[-1]]])
+        portion = df_r.iloc[s:]
+        s += n
+        portion['suma_acumulada'] = portion['valor'].cumsum()
+        df_rr = pd.concat([df_rr, portion.iloc[[-1]]])
+
+        ax1.plot(df_rr["fecha"], df_rr["suma_acumulada"])
+    else:
+        ax1.plot(dates_test, costes)
+    ax2.plot(dates_test, c_ascendente)
+    ax1.set_xlabel('Date')
+    ax2.set_xlabel('Date')
+    ax1.set_ylabel('€')
+    ax2.set_ylabel('€')
+    ax1.set_title('Predicted Cost')
+    ax2.set_title('Accumulated Predicted Cost')
+    fig.subplots_adjust(wspace=0.5)
+    plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
+    plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
 
     # Mostrar el gráfico en Streamlit
     st.pyplot(fig)
 
-    # # Sidebar - Selección de años
-    # st.sidebar.header('Selecciones del usuario')
-    # ano_min = st.sidebar.selectbox('Año comienzo', list(range(1950,2020)))
-    # ano_max = st.sidebar.selectbox('Año final', list(reversed(range(1950,2020))))
-    # ran_anual = list(range(ano_min, ano_max+1))
-
-    # # Sidebar - Selección variable
-    # dic_var = {'Horas anuales trabajadas por ocupados':'avh',
-    #            'Horas anuales trabajadas por semana':'avh_week',
-    #            'GDP del lado del gasto (millones)':'rgdpe',
-    #            'GDP del lado del gasto per capita':'rgdpe_cap',
-    #            'GDP del lado de la producción (millones)':'rgdpo',
-    #            'GDP del lado de la producción per capita':'rgdpo_cap',
-    #            'Población (millones)':'pop',
-    #            'Personas contratadas (millones)':'emp',
-    #            'Índice de capital humano':'hc',
-    #            'Nivel de ingresos o consumo diario':'inc_con',
-    #            'Días de vacaciones y festivos':'days_vac',
-    #            'Productividad':'prod',
-    #            'Índice de desarrollo humano':'idh',
-    #            'Posición en el World Happiness Report':'happ',
-    #            'PIB por hora trabajada':'GDP_hour',
-    #            'GDP per capita':'eco',
-    #            'Esperanza de vida':'life_exp',
-    #            'Libertad':'freed',
-    #            'Confianza en el Gobierno':'trust',
-    #            'Generosidad':'gen'}
-    # selected_var = st.sidebar.selectbox('Variables', dic_var.keys())
-
-    # # Acote años y variable
-    # df_anos = df[df.year.isin(ran_anual)]
-    # ev = df_anos.loc[:,['continent','country','year', dic_var[selected_var]]]
-    # ev.dropna(axis='rows', how='any', inplace=True)
-
-    # #Sidebar - Autoselección país por continente
-    # st.sidebar.markdown('Puedes elegir todos los países de uno o más continentes')
-    # sorted_unique_cont = sorted(ev.continent.unique())
-    # selected_continent = st.sidebar.multiselect('Continentes', sorted_unique_cont, [])
-    # ev_cont = ev[ev.continent.isin(selected_continent)]
-
-    # # Sidebar - Selección país
-    # st.sidebar.markdown('O escogerlos uno a uno')
-    # sorted_unique_country = sorted(ev.country.unique())
-    # al_selected_country = sorted(ev_cont.country.unique())
-    # selected_country = st.sidebar.multiselect('Países', sorted_unique_country, al_selected_country)
-
-    # # Acote país
-    # ev = ev[ev.country.isin(selected_country)]
-
-    # # Ploteo gráfico de líneas
-    # ev_lin = ev.pivot(index='year', columns='country', values=dic_var[selected_var])
-    # st.subheader('Gráfico de líneas')
-    # fig = ev_lin.iplot(asFigure=True, kind='line', title=selected_var)
-    # st.plotly_chart(fig)
-
-    # # Selección año para gráfico de barras
-    # st.subheader('Gráfico de barras')
-    # st.markdown('Elige el año en el que quieres comparar los países')
-    # selected_year = st.selectbox('Año para comparar', list(reversed(sorted(ev.year.unique()))))
-
-    # # Ploteo gráfico de barras
-    # ev_mask = ev['year']==selected_year
-    # ev_comp = ev[ev_mask]
-    # ev_bar = ev_comp.loc[:,['country', dic_var[selected_var]]]
-    # ev_bar = ev_bar.sort_values(by=dic_var[selected_var],ascending=False)
-    # ev_bar = ev_bar.set_index('country')
-    # bar = ev_bar.iplot(asFigure=True, kind='bar', xTitle='Países',yTitle=selected_var,color='blue')
-    # st.plotly_chart(bar)
-    # st.markdown('*Si al añadir un país, este no aparece, se debe a que no hay datos para ese año')
-
-    # # Selección variable para comparar
-    # st.subheader('Comparación de variables')
-    # st.markdown('Elige la variable con la que quieres comparar la variable seleccionada anteriormente en el año seleccionado')
-    # dic2 = dic_var.copy()
-    # del dic2[selected_var]
-    # var_comp = st.selectbox('Variable para comparar', dic2.keys())
-
-    # # Ploteo scatter
-    # ev_scat = df_anos.loc[:,['continent','country','year',dic_var[selected_var],dic_var[var_comp]]]
-    # ev_mask_scat = ev_scat['year']==selected_year
-    # ev_scat = ev_scat[ev_mask_scat]
-    # ev_scat = ev_scat[ev_scat.country.isin(selected_country)]
-    # ev_scat.dropna(axis='rows', how='any', inplace=True)
-    # scat = px.scatter(ev_scat,x=dic_var[selected_var],y=dic_var[var_comp],color='continent',hover_name='country')
-    # st.plotly_chart(scat)
-    # st.markdown('*Si al añadir un país, este no aparece, se debe a que no hay datos para ese año')
+    st.markdown(f"COSTE TOTAL: {c_total}€")
