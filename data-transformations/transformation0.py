6227557bfa9e7046a7391e7e7057b50f7187b45d
JavierLuqueSaiz <167310505+JavierLuqueSaiz@users.noreply.github.com>
2024-05-21 13:33:23 +0200

    Add files via upload

---
+++ data-transformations/transformation0.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+df = pd.read_csv('HomeC.csv', delimiter=',', header=0,)  # Ajusta el delimitador y el tipo de datos según tu archivo
+
+start_time = datetime(2016, 1, 1, 0, 0, 0)  # 1 de enero de 2016 a las 5:00 AM
+
+df['time'] = start_time + pd.to_timedelta(df.index, unit='m')
+
+df.head()
+
+df.to_csv('data0.csv', index=False,sep=",")
